1b96c5453d7e5f933ee82fb316f40faf83829ef0
Patrick West <westp@rpi.edu>
2017-04-29 17:07:41 -0600

    Tests that need to be created

---
+++ src/test/java/edu/rpi/tw/vsto/VstoClassTypeTests.java
@@ -0,0 +1,25 @@
+package edu.rpi.tw.vsto;
+
+import org.junit.Assert;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+//import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(VstoWebServicesApplication.class)
+public class VstoClassTypeTests
+{
+
+    private static final Logger log = LoggerFactory.getLogger(VstoClassTypeTests.class);
+
+    @Test
+    public void classesTest() {
+    }
+}
